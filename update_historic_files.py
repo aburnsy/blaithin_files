a0c1d74656a9206edf5fa7417645a52ed8b97016
Andy Burns <andrewburns89@gmail.com>
2024-04-05 14:55:04 +0100

    try again with historic pqs

--- update_historic_files.py
+++ update_historic_files.py
@@ -2,6 +2,7 @@ import polars as pl
 import os
 from pathlib import Path
 from datetime import datetime
+import cloud_storage
 
 folder_path = "data/gardens4you/"
 
@@ -17,7 +18,11 @@ for file_found in Path(folder_path).glob("*.parquet"):
     date_value = file_found.name.split(".")[0]
     if date_value == max_date:
         continue
-    print(date_value)
-    df = df.with_columns((pl.lit(date_value)).alias("input_date"))
+    df = cloud_storage.add_defaults_to_fields(
+        df,
+        field_name="input_date",
+        default_value=datetime.strptime(date_value, "%Y-%m-%d"),
+    )
     print(df.sample(5))
+    print(df.dtypes)
     df.write_parquet(f"{folder_path}/{date_value}.parquet")
