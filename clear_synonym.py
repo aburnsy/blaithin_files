acf228af3cb4f823364f1d44bd379b79dcfebf64
Andy Burns <andrewburns89@gmail.com>
2024-04-08 19:04:01 +0100

    fix rhs data

--- clear_synonym.py
+++ clear_synonym.py
@@ -5,12 +5,30 @@ from pathlib import Path
 lazydf = []
 
 basepath = Path("data/rhs/")
+# for myfile in basepath.iterdir():
+#     length = (
+#         pl.scan_parquet(myfile)
+#         .select(["botanical_name", "plant_type"])
+#         .filter(pl.col("botanical_name").str.starts_with("Deschampsia"))
+#         .filter(~pl.col("plant_type").list.contains("Grass Like"))
+#         .select(pl.len())
+#         .collect()
+#         .item()
+#     )
+#     if length == 0:
+#         continue
+#     else:
+#         df = pl.scan_parquet(myfile).select(["botanical_name", "plant_type"]).collect()
+#         print(df)
+#         # print(f"Removing file {myfile.name}")
+#         file_path = myfile.resolve()
+#         os.remove(file_path)
+
 for myfile in basepath.iterdir():
     length = (
         pl.scan_parquet(myfile)
         .select(["botanical_name", "plant_type"])
-        .filter(pl.col("botanical_name").str.starts_with("Deschampsia"))
-        .filter(~pl.col("plant_type").list.contains("Grass Like"))
+        .filter(pl.col("plant_type").list.contains("G"))
         .select(pl.len())
         .collect()
         .item()
@@ -18,15 +36,8 @@ for myfile in basepath.iterdir():
     if length == 0:
         continue
     else:
-        df = (
-            pl.scan_parquet(myfile)
-            .select(["botanical_name", "plant_type"])
-            .filter(pl.col("botanical_name").str.starts_with("Deschampsia"))
-            .collect()
-        )
+        df = pl.scan_parquet(myfile).select(["botanical_name", "plant_type"]).collect()
         print(df)
         # print(f"Removing file {myfile.name}")
         file_path = myfile.resolve()
         os.remove(file_path)
-
-# 62755
